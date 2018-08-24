a89841d7c0b64e756fa1e30a7556a5bf25dc7de8
shullmb <shullmb@gmail.com>
2018-08-24 10:54:45 -0700

    update to skills page

--- src/Skill.jsx
+++ src/Skill.jsx
@@ -4,7 +4,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 export const Skill = props => {
   return (
     <div className="skill">
-      <FontAwesomeIcon icon={props.icon} size='6x' />
+      <FontAwesomeIcon icon={props.icon} size={`${props.size}x`} />
       <h4>{props.skill}</h4>
     </div>
   )
--- src/Skills.jsx
+++ src/Skills.jsx
@@ -1,22 +1,55 @@
 import React from 'react';
 import { Skill } from './Skill';
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faGem, faAlignRight, faCloud } from '@fortawesome/free-solid-svg-icons'
+import { faGem,
+         faAlignRight,
+         faBold,
+         faCloud,
+         faAsterisk,
+         faSpaceShuttle,
+         faRuler,
+         faIndent, 
+         faDollarSign, 
+         faServer} from '@fortawesome/free-solid-svg-icons'
 import { fab } from '@fortawesome/free-brands-svg-icons'
 import data from './data/skills.json';
 
-library.add(fab, faGem, faAlignRight, faCloud)
+library.add(fab, faGem, faAlignRight, faBold, faCloud, faAsterisk, faSpaceShuttle, faRuler, faIndent, faDollarSign, faServer)
 
 export const Skills = props => {
   const skills = data.map( (skill, i) => {
     let icon = skill.set === 'fab' ? [skill.set, skill.icon] : skill.icon
-    return (
-      <Skill key={`${skill.skill}${i}`}skill={skill.skill} icon={icon} />
-    )
+    if ( !skill.lib && !skill.tool ) {
+      return (
+        <Skill key={`${skill.skill}${i}`}skill={skill.skill} icon={icon} size={6} />
+      )
+    }
   })
+  const tools = data.map((skill, i) => {
+    let icon = skill.set === 'fab' ? [skill.set, skill.icon] : skill.icon
+    if (skill.tool) {
+      return (
+        <Skill key={`${skill.skill}${i}`} skill={skill.skill} icon={icon} size={4} />
+      )
+    }
+  })
+  const libraries = data.map((skill, i) => {
+    let icon = skill.set === 'fab' ? [skill.set, skill.icon] : skill.icon
+    if (skill.lib) {
+      return (
+        <Skill key={`${skill.skill}${i}`} skill={skill.skill} icon={icon} size={4} />
+      )
+    }
+  })
+
   return (
     <div>
+      <h2>The following represents tech and tools that I am currently comfortable with or have used extensively in the past.</h2>
       {skills}
+      <h3>libraries and frameworks:</h3>
+      {libraries}
+      <h3>tools:</h3>
+      {tools}
     </div>
   )
 }
